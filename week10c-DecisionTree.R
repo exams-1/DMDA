3236e8fb8bfbab16d19d6ca270debb7a003ac4b3
exams-1 <161727693+exams-1@users.noreply.github.com>
2024-12-11 21:31:21 +0530

    Update week10c-DecisionTree.R

--- week10c-DecisionTree.R
+++ week10c-DecisionTree.R
@@ -1,14 +1,17 @@
+
 data("iris")
 install.packages("caret")
 install.packages("C50")
 library(caret)
 library(C50)
 set.seed(7)
-inTraininglocal<-createDataPartition(iris$Species,p=.70,list=F)
-training<-iris[inTraininglocal,]
-testing<-iris[-inTraininglocal,]
-model<-C5.0(Species ~ .,training)
+inTraininglocal <- createDataPartition(iris$Species, p = .70, list = FALSE)
+training <- iris[inTraininglocal, ]
+testing <- iris[-inTraininglocal, ]
+model <- C5.0(Species ~ ., data = training)
 summary(model)
-pred<-predict.C5.0(model,testing[,-5])
-a<-tables(testing$Species,pred)
+pred <- predict(model, testing[, -5])  # use predict() instead of predict.C5.0()
+a <- table(testing$Species, pred)
+accuracy <- sum(diag(a)) / sum(a)
+print(accuracy)
 plot(model)
